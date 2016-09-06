142d7b9c40b34be744d49a318889c67906fb594f
Robin <robmarkcole@users.noreply.github.com>
2016-09-06 09:55:36 +0100

    Update List_files.py

--- List_files.py
+++ List_files.py
@@ -1,7 +1,6 @@
 import os
 f = open("file_list.txt","a") #opens file with name of "file_list.txt", creates if doesnt exist
-for file in os.listdir("/Users/robincole/Dropbox/09 Med phys/NPL files/Pinnacle/RD_DW"):
-    #print(file)
+for file in os.listdir("/Users/path_to_folder"):
     f.write(file)
     f.write("\n")
 f.close()
