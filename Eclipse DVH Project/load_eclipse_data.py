4596b4975e3f5301890fbe6dbaf68100c9184b00
robmarkcole <robmarkcole@gmail.com>
2016-10-07 18:18:11 +0100

    Added some R

--- Eclipse DVH Project/load_eclipse_data.py
+++ Eclipse DVH Project/load_eclipse_data.py
@@ -5,9 +5,9 @@ Created on Mon Oct  3 13:57:25 2016
 @author: robincole
 """
 
- import numpy as np
- file = 'case1_AAA.txt'
+import numpy as np
+file = 'case1_AAA.txt'
  
  # data = np.genfromtxt(file, dtype=None) # for column data
  
- np.genfromtxt(file, dtype='str', max_rows=15, delimiter='@') 
+header =  np.genfromtxt(file, dtype=None, max_rows=6, delimiter=':') 
