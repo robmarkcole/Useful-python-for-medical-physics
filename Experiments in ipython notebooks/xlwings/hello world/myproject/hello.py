9aa989b380b207f974bdc961c6103ce34e6b2eaf
robmarkcole <robmarkcole@gmail.com>
2017-10-17 06:59:41 +0100

    Cleanout non medical physics code

--- Experiments in ipython notebooks/xlwings/hello world/myproject/hello.py
+++
@@ -1,7 +0,0 @@
-# hello.py
-import numpy as np
-import xlwings as xw
-
-def world():
-    wb = xw.Book.caller()
-    wb.sheets[0].range('A1').value = 'Hello World!'
--- General scrips/List_files_basic.py
+++
@@ -1,6 +0,0 @@
-import os
-f = open("file_list.txt","a") #opens file with name of "file_list.txt", creates if doesnt exist
-for file in os.listdir("/Users/path_to_folder"):
-    f.write(file)
-    f.write("\n")
-f.close()
